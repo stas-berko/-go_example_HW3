902f484d563adf9c2e140cb0b891ca1e24f235ca
Stanislav Berko <uselesstas@gmail.com>
2020-06-03 23:36:29 +0300

    [add] project code

--- run.go
+++ run.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 var step = map[int][2]int{
 	0: {1, 0},
@@ -13,26 +16,56 @@ func addIndex(ind1 [2]int, ind2 [2]int) [2]int {
 	return [2]int{ind1[0] + ind2[0], ind1[1] + ind2[1]}
 }
 
-func getS(cnt, stepIndex int, currIndex [2]int, stor [][2]int) [][2]int {
+func getS(cnt, stepIndex int, currIndex [2]int, c chan [2]int) {
 	if cnt <= 0 {
-		return stor
+		close(c)
+		return
 	}
 	inc := step[stepIndex]
 
 	for indexCnt := cnt; indexCnt > 0; indexCnt-- {
 		currIndex = addIndex(inc, currIndex)
-		stor = append(stor, currIndex)
+		c <- currIndex
 	}
 
 	if stepIndex%2 == 1 {
 		cnt--
 	}
-	return getS(cnt, (stepIndex+1)%4, currIndex, stor)
+	getS(cnt, (stepIndex+1)%4, currIndex, c)
+}
+
+func getSequence(n int) chan [2]int {
+	c := make(chan [2]int)
+	go getS(n, 3, [2]int{0, -1}, c)
+	return c
+}
+
+func generateArr(n int, random bool) [][]int {
+	val := 0
+	arr := make([][]int, n)
+	for i := range arr {
+		arr[i] = make([]int, n)
+		for j := range arr[i]{
+			if random{
+				arr[i][j] = rand.Intn(100)
+			}else {
+				arr[i][j] = val
+				val++
+			}
+		}
+	}
+
+	return arr
 }
 
 func main() {
-	n := 4
-	sequence := make([][2]int, 0, n*n)
-	fmt.Println(getS(n, 3, [2]int{0, -1}, sequence))
+	n := 5
+	resArr:= make([]int, 0, n*n)
+	arr := generateArr(n, false)
+	fmt.Println(arr)
 
+	for index := range getSequence(n){
+		resArr = append(resArr, arr[index[0]][index[1]])
+	}
+	fmt.Println(resArr)
 }
