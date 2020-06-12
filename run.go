b3259e339ed196aff4a06acffa8f27af416ac2c7
Stanislav Berko <uselesstas@gmail.com>
2020-06-13 01:28:58 +0300

    [fix] add square matrix validation

--- run.go
+++ run.go
@@ -45,10 +45,10 @@ func generateArr(n int, random bool) [][]int {
 	arr := make([][]int, n)
 	for i := range arr {
 		arr[i] = make([]int, n)
-		for j := range arr[i]{
-			if random{
+		for j := range arr[i] {
+			if random {
 				arr[i][j] = rand.Intn(100)
-			}else {
+			} else {
 				arr[i][j] = val
 				val++
 			}
@@ -57,15 +57,34 @@ func generateArr(n int, random bool) [][]int {
 
 	return arr
 }
+func isSquareMatrix(matr [][]int) bool {
+	init_cap := cap(matr)
+	for _, arr := range matr {
+		if init_cap != cap(arr) {
+			return false
+		}
+	}
+	return true
+}
 
-func main() {
-	n := 5
-	resArr:= make([]int, 0, n*n)
-	arr := generateArr(n, false)
-	fmt.Println(arr)
+func getTornadoMatrix(arr [][]int, cap int) []int {
+	resArr := make([]int, 0, cap*cap)
 
-	for index := range getSequence(n){
+	for index := range getSequence(cap) {
 		resArr = append(resArr, arr[index[0]][index[1]])
 	}
-	fmt.Println(resArr)
+	return resArr
+}
+
+func main() {
+	arr := generateArr(5, false)
+	fmt.Println(arr)
+
+
+	if isSquareMatrix(arr) {
+		resArr:= getTornadoMatrix(arr, cap(arr))
+		fmt.Println(resArr)
+	} else {
+		fmt.Println("Invalid matrix format")
+	}
 }
